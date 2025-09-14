996b2d56a19884625fec6edae6f13904b6738b65
Priyamshu11 <priyamshumk@gmail.com>
2025-09-14 17:57:58 +0530

    corrected experience service

--- experience-service/src/main/java/com/example/client/UserServiceClient.java
+++ experience-service/src/main/java/com/example/client/UserServiceClient.java
@@ -1,12 +1,13 @@
 package com.example.client;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", path = "/user")
+@FeignClient(name = "auth-service", path = "/api/auth")
 public interface UserServiceClient {
 
-    @GetMapping("/exists/{id}")
+    @GetMapping(value="/users/{id}/exists",produces = "application/json")
     Boolean userExists(@PathVariable("id") Long userId);
 }
