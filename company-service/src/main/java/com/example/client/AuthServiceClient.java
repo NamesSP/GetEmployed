4bb068775f04a536f2f37bd95d50a9ff586e7c61
NamesSP <sppratham420@gmail.com>
2025-09-17 12:17:11 +0530

    corrected job-service. added authslient to other services where absent. added feigninterceptor for job-service and user-service

---
+++ company-service/src/main/java/com/example/client/AuthServiceClient.java
@@ -0,0 +1,30 @@
+package com.example.client;
+
+
+import com.example.dto.AuthUserInfoDto;
+import com.example.dto.ValidateTokenResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "auth-service", path = "/api/auth")
+public interface AuthServiceClient {
+    @GetMapping(value = "/user-info/{username}", produces = "application/json")
+    AuthUserInfoDto getUserInfoByUsername(@PathVariable("username") String username);
+
+    @GetMapping(value = "/users/{id}/exists", produces = "application/json")
+    Boolean userExists(@PathVariable("id") Long id);
+
+//    @GetMapping(value = "/users/{id}/info", produces = "application/json")
+//    AuthUserInfoDto getUserInfoById(@PathVariable("id") Long id);
+
+    @GetMapping(value = "/users/info/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    AuthUserInfoDto getUserInfoById(@PathVariable("id") Long id);
+
+
+}
+
+
