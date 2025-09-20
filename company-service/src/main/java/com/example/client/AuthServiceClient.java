e094844f35c47cb60e1cc01c57a8c047d811e959
SP <sppratham420@gmail.com>
2025-09-20 12:47:19 +0530

    trying to restructure auth service

--- company-service/src/main/java/com/example/client/AuthServiceClient.java
+++ company-service/src/main/java/com/example/client/AuthServiceClient.java
@@ -1,13 +1,13 @@
 package com.example.client;
 
-
 import com.example.dto.AuthUserInfoDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "auth-service", path = "/api/auth")
+// @FeignClient(name = "auth-service", path = "/api/auth")
+@FeignClient(name = "sonyapigateway", path = "/api/auth")
 public interface AuthServiceClient {
     @GetMapping(value = "/user-info/{username}", produces = "application/json")
     AuthUserInfoDto getUserInfoByUsername(@PathVariable("username") String username);
@@ -15,13 +15,10 @@ public interface AuthServiceClient {
     @GetMapping(value = "/users/{id}/exists", produces = "application/json")
     Boolean userExists(@PathVariable("id") Long id);
 
-//    @GetMapping(value = "/users/{id}/info", produces = "application/json")
-//    AuthUserInfoDto getUserInfoById(@PathVariable("id") Long id);
+    // @GetMapping(value = "/users/{id}/info", produces = "application/json")
+    // AuthUserInfoDto getUserInfoById(@PathVariable("id") Long id);
 
     @GetMapping(value = "/users/info/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     AuthUserInfoDto getUserInfoById(@PathVariable("id") Long id);
 
-
 }
-
-
